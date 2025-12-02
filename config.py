a0acc60c8d41cb28e6d64983dffb79c9fd18b48e
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-12-02 07:24:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,17 +9,17 @@ OWNER_ID = 7654385403
 MSG_EFFECT = 5046509860389126442
 
 SHORT_URL = "shortxlinks.com/" # shortner url 
-SHORT_API = "64d631b036df348caab852591a09288cbf5b6809" 
+SHORT_API = "" 
 SHORT_TUT = "https://t.me/+wekKcN1tjbAxY2U1"
 
 # Bot Configuration
 SESSION = "Kaya"
-TOKEN = "7792842081:AAHXJGLGX-vGwbOAjidVXLLQlcu_HO5JrvU"
+TOKEN = ""
 API_ID = "29245477"
 API_HASH = "0abc83883262245c90ca337b7a0375c4"
 WORKERS = 5
 
-DB_URI = "mongodb+srv://phrolovaxrobot:p0SFEz825QuqzwTi@cluster0.sy26fqm.mongodb.net/?retryWrites=true&w=majority"
+DB_URI = ""
 DB_NAME = "cluster0"
 
 FSUBS = [[-1001457313028, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
